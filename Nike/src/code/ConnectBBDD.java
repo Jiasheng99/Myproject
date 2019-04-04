02986dfa0dfda985d6a06a51540cead4209f7d53
windows10 <windows10@LAPTOP-EPOUB0SR>
2019-04-04 20:42:55 +0200

    I change the sistem.err.println to log

--- Nike/src/code/ConnectBBDD.java
+++ Nike/src/code/ConnectBBDD.java
@@ -2,23 +2,22 @@ package code;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class ConnectBBDD {
 
 	static String DRIVER="org.hsqldb.jdbcDriver";
 	static String URL="jdbc:hsqldb:C:\\Users\\pc10\\eclipse-workspace\\Nike\\WebContent\\WEB-INF\\db\\srv_db.tmp";
+	private final static Logger LOGGER = Logger.getLogger("bitacora.subnivel.Control");
 	public static Connection getConnection() {
 		Connection con = null;
 		try {
 			Class.forName(DRIVER);
 			con=DriverManager.getConnection(URL,"sa", "");
 		}catch(ClassNotFoundException | SQLException e) {
-			System.err.println("Error: "+e);
+			LOGGER.log(Level.INFO, "Error: "+e);
 		}
 		return con;
 	}
 	
-	public ConnectBBDD() {
-		super();
-	}
 }
